8ad60b24438baed3ba6991de34b72af084e7b401
Rasmus Skriver Møller <m78578_WADM@yousee.dk>
2024-06-28 14:02:11 +0200

    kommentar i vimwiki filen i forhold til ændring af path

--- lua/theprimeagen/lazy/vimwiki.lua
+++ lua/theprimeagen/lazy/vimwiki.lua
@@ -1,3 +1,9 @@
 return {
     'vimwiki/vimwiki'
+    -- ændring af vim wiki path så den kommer med i git commit, det virker dog ikke så skal undersøge nærmere på et tidspunkt.
+--    config = function ()
+--    vim.cmd([[
+--    let g:vimwiki_list = [{'path': 'C:\\Users\\m78578_wadm\\AppData\\Local\\nvim\\my_vimwiki\\', 'path_html': 'C:\\Users\\m78578_wadm\\AppData\\Local\\nvim\\my_vimwiki_html\\'}]
+-- ]])
+--    end
 }
